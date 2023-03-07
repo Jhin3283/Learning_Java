6f5efe4c9ce832bca74e536eecd0de41e9d3dfee
jhin3283 <hj3283@gmail.com>
2023-03-07 14:39:29 +0900

    STUDY: Component

--- learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/App03GamingSpringBeans.java
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/App03GamingSpringBeans.java
@@ -1,19 +1,21 @@
 package com.in28minutes.learnspringframework;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 import com.in28minutes.learnspringframework.game.GameRunner;
 import com.in28minutes.learnspringframework.game.GamingConsole;
-import com.in28minutes.learnspringframework.game.MarioGame;
-import com.in28minutes.learnspringframework.game.PacMan;
-import com.in28minutes.learnspringframework.game.SuperContraGame;
 
+@Configuration
+@ComponentScan("com.in28minutes.learnspringframework.game")
 public class App03GamingSpringBeans {
-
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		try(var context = 
-				new AnnotationConfigApplicationContext(GamingConfiguration.class)){			
+				new AnnotationConfigApplicationContext(App03GamingSpringBeans.class)){			
 			context.getBean(GamingConsole.class).up();
 			context.getBean(GameRunner.class).run();
 		}
